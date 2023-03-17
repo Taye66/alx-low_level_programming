29bbfbbb9b3d3c50e8c0748b9684e89c40f95842
Taye <Tayevictor46@gmail.com>
2023-03-16 23:20:01 -0700

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 
 
 /**
@@ -11,6 +10,6 @@
 
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
