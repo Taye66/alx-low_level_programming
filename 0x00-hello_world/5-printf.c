06164338f7a814228493deb70229f88c57e13857
Taye <Tayevictor46@gmail.com>
2023-03-16 22:07:59 -0700

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function for a c program
+ *
+ * Return: 0 when succesful
+ */
+
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
