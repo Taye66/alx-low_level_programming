e9443156e32eef79192c3c7aec4e33dccc28c273
Taye <Tayevictor46@gmail.com>
2023-03-16 23:05:40 -0700

    Unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - serves as the starting point for program execution
+ *
+ * Return: 1 when sucessful
+ */
+
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
